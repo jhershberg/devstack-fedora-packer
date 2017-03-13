e1d1a4dd94692b7b7c5f9861f1c83a3d8249a6db
Michael Vorburger <mike@vorburger.ch>
2017-03-13 17:57:27 +0100

    iptables port 6080 ACCEPT, for novnc to actually work

--- devstack-setup.sh
+++ devstack-setup.sh
@@ -5,8 +5,13 @@ sudo mkdir /opt/stack
 sudo chmod 777 /opt/stack
 sudo mkdir -p /opt/logs/stack
 sudo chmod 777 /opt/logs/stack
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+sudo systemctl stop    iptables.service
+sudo systemctl disable iptables.service
 git clone https://github.com/openstack-dev/devstack.git
 cd devstack
 git checkout stable/newton
+./stack.sh
+sudo iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 6080 -j ACCEPT
+sed -i -r -e 's/^#*\s*(OFFLINE=).*$/\1True/' local.conf
+sed -i -r -e 's/^#*\s*(RECLONE=).*$/\1False/' local.conf
+
